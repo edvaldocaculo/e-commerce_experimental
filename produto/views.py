a69634fe294ee362fe4ac8248b045e3510f63d5d
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2023-06-01 00:18:12 -0300

    views produto

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import render
+from django.views.generic import ListView, View
 
-# Create your views here.
+
+class ListaProduto(ListView):
+
+    def get(*args, **kwargs):
+        return HttpResponse('produto')
+
+
+class DetalhesProduto(View):
+    def get(*args, **kwargs):
+        return HttpResponse('detalhe produto')
+
+
+class AdicionarAoCarrinho(View):
+    def get(*args, **kwargs):
+        return HttpResponse('adicionar produto')
+
+
+class RemoverDoCarrinho(View):
+    def get(*args, **kwargs):
+        return HttpResponse('remver')
+
+
+class Carrinho(View):
+    def get(*args, **kwargs):
+        return HttpResponse('carrinho')
+
+
+class Finalizar(View):
+    def get(*args, **kwargs):
+        return HttpResponse('finalizar')
