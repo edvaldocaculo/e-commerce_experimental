0d99323fff4651ba53f90216910ed07b432f31e1
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2023-06-01 00:17:40 -0300

    views perfil

--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class Criar(View):
+    def get(*args, **kwargs):
+        return HttpResponse('Criar')
+
+
+class Atualizar(View):
+    def get(*args, **kwargs):
+        return HttpResponse('Atualizar')
+
+
+class Login(View):
+    def get(*args, **kwargs):
+        return HttpResponse('Login')
+
+
+class Logout(View):
+    def get(*args, **kwargs):
+        return HttpResponse('Logout')
